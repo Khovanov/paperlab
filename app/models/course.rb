f0e64f06a1c0e30e352844a9aaf3884f1b3ae060
Ubuntu <ubuntu@rails-dev-box>
2017-02-05 21:49:40 +0300

    inistall Sidekiq

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ApplicationRecord
   has_many :materials, dependent: :destroy
 
   def duplicate
-    CourseDuplicationJob.perform_now(self)
+    # CourseDuplicationJob.perform_now(self)
+    job = CourseDuplicationJob.perform_later(self)
+    job.provider_job_id
   end
 end
