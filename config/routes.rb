bb07cdd953232fc77df0228c494498c37c2c082a
Ubuntu <ubuntu@rails-dev-box>
2017-02-05 20:44:56 +0300

    create Course resourse

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'courses#index'
+  resources :courses, except: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
